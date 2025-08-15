821df07e762a3fea7f960fc17de27fc2570f3218
unknown <satvikc73@gmail.com>
2025-08-15 22:57:48 +0530

    insecure webpush

--- api/config.js
+++
@@ -1,6 +0,0 @@
-// api/config.js
-
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/javascript');
-  res.status(200).send(`window.env = { SUPABASE_URL: '${process.env.SUPABASE_URL}', SUPABASE_ANON_KEY: '${process.env.SUPABASE_ANON_KEY}' };`);
-}
